f34b6d803b5a356d88bfcf24f84f637425ca1fe1
Max <maximer.2006@gmx.de>
2024-08-10 19:24:28 +0200

    new camera param

--- Scene/World.gd
+++ Scene/World.gd
@@ -22,10 +22,17 @@ func _process(delta):
 
 
 func move_camera() -> void:
+	const LEFT_X_CAP: int = 0
+	const RIGHT_X_CAP: int = 10000
 	var player_horizontal_position: int = Player.get_transform().get_origin().x
 	var camera_horizontal_position: int = Camera.get_transform().get_origin().x
 	
 	var difference: int = abs(player_horizontal_position - camera_horizontal_position)
 	
-	var smoothed_horizontal_movement = move_toward(camera_horizontal_position, player_horizontal_position, sqrt(difference))
+	var smoothed_horizontal_movement = move_toward(camera_horizontal_position, player_horizontal_position, 0.7 * sqrt(difference))
+	if smoothed_horizontal_movement < LEFT_X_CAP:
+		smoothed_horizontal_movement = LEFT_X_CAP
+	elif smoothed_horizontal_movement > RIGHT_X_CAP:
+		smoothed_horizontal_movement = RIGHT_X_CAP
+	
 	Camera.transform.origin = Vector2(smoothed_horizontal_movement, 0)
